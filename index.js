5083f8ef544e93dc4c0120f52a2be6783935404d
daggubatibhargavi <daggubatibhargavi684@gmail.com>
2024-10-21 21:25:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+// let str = "Hello World!";Relace the substring “World!” with “My Dear Friend” ..Write the program for that
+let str = "Hello World!";
+replaceString = str.replace("World!", "My Dear Friend");
+console.log(replaceString);
+//let a=”Hungry”;Repeat the “Hungry” for 10 timesWrite the program for that
+let a = "Hungry";
+repeatString = a.repeat(10);
+console.log(repeatString);
+//let sentence = "The quick brown fox jumps over the lazy dog. The fox is clever.";Find the last index of the string “fox”
+let sentence =
+  "The quick brown fox jumps over the lazy dog. The fox is clever.";
+lastIndex = sentence.lastIndexOf("fox");
+console.log(lastIndex);
+//let message = "Hello, world!";Find the string starts with “hi”Write code for that
+let message = "Hello, world!";
+startWith = message.startsWith("hi");
+console.log(startWith);
+//let fileName = "document.pdf";Find the string ends with “.pdf”Write code for that
+let fileName = "document.pdf";
+endsWith=fileName.endsWith(".pdf");
+console.log(endsWith);
+
+
